77ab906bb3cbeaf32406333ac6e677b77b4c78a5
Mark Raasveldt <mark.raasveldt@gmail.com>
2017-01-12 16:14:11 +0100

    Support file drag/drop on OSX as well.

--- PantherGUI/PGWindow.h
+++ PantherGUI/PGWindow.h
@@ -8,6 +8,13 @@
 
 @interface PGNSWindow : NSWindow
 
--(void)close;
+- (instancetype)initWithContentRect:(NSRect)contentRect 
+                          styleMask:(NSUInteger)style 
+                            backing:(NSBackingStoreType)bufferingType 
+                              defer:(BOOL)flag;
 
+-(void)close;
+-(NSDragOperation)draggingEntered:(id<NSDraggingInfo>)sender;
+-(NSDragOperation)draggingUpdated:(id<NSDraggingInfo>)sender;
+-(BOOL)performDragOperation:(id<NSDraggingInfo>)sender;
 @end
