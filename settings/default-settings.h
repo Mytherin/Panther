0f98be2184b1b71f7a746c8f418e6bfef12efc17
Mark Raasveldt <mark.raasveldt@gmail.com>
2017-06-27 09:21:12 +0200

    CMake build system.

--- settings/default-settings.h
+++ settings/default-settings.h
@@ -10,7 +10,7 @@ const char* PANTHER_DEFAULT_SETTINGS = R"DEFAULTSETTINGS(
 	"word_wrap" : "auto",
 	"hot_exit" : true,
 	"automatic_reload_threshold" : 1000,
-	"default_terminal" : "C:\\Program Files\\Git\\git-bash.exe",
+	"default_terminal" : "C:\\Proqgram Files\\Git\\git-bash.exe",
 	"ignored_files": ["*.exe", "*.app", "*.dll", "*.so", "*.dylib", "*.o", "*.O", "*.obj", "*.pyc",
 					  "*.ttf", "*.sys", "*.msi", "*.jpg", "*.jpeg", "*.png", "*.bmp", "*.ico", "*.mp3",
 					  "*.wav", "*.mp4", "*.db", "*.sdf"]
