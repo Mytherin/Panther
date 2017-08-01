6edac63355b5e8f3d7730f0f4b172bf1d580340b
Mytherin <mark.raasveldt@gmail.com>
2017-08-01 23:27:00 +0200

    Syntect part 3

--- rust/include/rust/syntax.h
+++ rust/include/rust/syntax.h
@@ -3,5 +3,17 @@
 
 extern "C" {
 	typedef void* PGSyntaxState;
-	
+	typedef void* PGSyntaxParser;
+
+
+	extern PGSyntaxParser PGCreateParser();
+	extern void PGDestroyParser(PGSyntaxParser parser);
+
+	extern PGSyntaxState PGGetDefaultState(PGSyntaxParser parser);
+	extern PGParserState PGCopyParserState(PGSyntaxState state);
+	extern void PGDestroyParserState(PGSyntaxState state);
+
+	extern int PGStateEquivalent(PGSyntaxState a, PGSyntaxState b);
+
+	extern PGSyntaxState PGParseLine(PGSyntaxParser parser, PGSyntaxState state, const char* line);
 }
