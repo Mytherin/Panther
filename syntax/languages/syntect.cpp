bca768afaa72993c2de59e7b30c20f20dc263f6b
Mark Raasveldt <mark.raasveldt@gmail.com>
2017-08-02 17:00:05 +0200

    More wrapper code.

--- syntax/languages/syntect.cpp
+++ syntax/languages/syntect.cpp
@@ -1,28 +1,48 @@
 
 #include "syntect.h"
-#include "rust/syntax.h"
 
 
 struct SyntectParserState {
-	PGParserSyntectState state;
+	PGSyntaxState state;
 };
 
-PGParserState SyntectHighlighter::IncrementalParseLine(TextLine& line, lng linenr, PGParserState state, PGParseErrors& errors, PGSyntax& syntax) {
+SyntectHighlighter::SyntectHighlighter() {
+	this->parser = PGCreateParser();
+}
 
+SyntectHighlighter::~SyntectHighlighter() {
+	PGDestroyParser(this->parser);
+}
+
+PGParserState SyntectHighlighter::IncrementalParseLine(TextLine& line, lng linenr, PGParserState state, PGParseErrors& errors, PGSyntax& syntax) {
+	SyntectParserState* s = static_cast<SyntectParserState*>(state);
+	PGParserState _new = PGParseLine(this->parser, s->state, line.GetLine());
+	return _new;
 }
 
 PGParserState SyntectHighlighter::GetDefaultState() {
-
+	SyntectParserState* s = new SyntectParserState();
+	s->state = PGGetDefaultState(this->parser);
+	return (PGParserState) s;
 }
 
 PGParserState SyntectHighlighter::CopyParserState(const PGParserState state) {
-
+	SyntectParserState* s = static_cast<SyntectParserState*>(state);
+	SyntectParserState* _new = new SyntectParserState();
+	_new->state = PGCopyParserState(s->state);
+	return _new;
 }
 
 bool SyntectHighlighter::StateEquivalent(const PGParserState a, const PGParserState b) {
-
+	SyntectParserState* _a = static_cast<SyntectParserState*>(a);
+	SyntectParserState* _b = static_cast<SyntectParserState*>(b);
+	return PGStateEquivalent(_a->state, _b->state);
 }
 
 void SyntectHighlighter::DeleteParserState(PGParserState state) {
+	if (!state) return;
 
+	SyntectParserState* s = static_cast<SyntectParserState*>(state);
+	PGDestroyParserState(s->state);
+	delete s;
 }
